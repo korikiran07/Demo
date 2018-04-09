149b2e631b288dc797432e80e3715e3a2c72411c
macadmin <korikiran07@gmail.com>
2018-04-09 16:57:36 +0530

    New

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,32 +1,49 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Webkit;
+using System.Collections.Generic;
+using Android.Util;
 
 namespace Demo
 {
-	[Activity(Label = "Demo", MainLauncher = true, Icon = "@mipmap/icon")]
+	[Activity(Label = "Demo", MainLauncher = false, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		WebView webview;
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
-
-			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
-
-			button.Click += delegate { button.Text = $"{count++} clicks!"; };
-
-			//kiran
+			FnInitialization();
+			FnCliCKEvent();
 		}
 
 		void FnInitialization()
-		{ 
+		{
+			webview = FindViewById<WebView>(Resource.Id.webView1);
+
+			try
+			{
+				webview.LoadUrl("https://developer.xamarin.com/recipes/android/controls/webview/load_local_content/");
+
+
+				//List<int> list = new List<int>();
+
+				//int kjhf = list[0];
+
+				//int a = 10;
+				//int b = 0;
+
+				//int x = a/b;
+			}
+			catch (System.Exception ex)
+			{
+				throw ex;
+			}
+			//webview.LoadUrl(string.Format("javascript: {0}", script));
 		
 		}
 
